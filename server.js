1f914b5e21907540ee1513186d817ad6d359de44
yaroslav12-creator <72810085+yaroslav12-creator@users.noreply.github.com>
2021-07-14 17:19:20 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+//backend
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+//Set static folder
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const botName = 'ChatCord Bot  ';
+
+//Run when client connects
+io.on('connection', (socket) => {
+    
+    socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);   
+        socket.join(user.room);
+
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+        socket.broadcast
+            .to(user.room)
+            .emit('message', formatMessage(botName, `${username} has joined the chat`));
+        
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        
+        io.to(user.room)
+          .emit('message', formatMessage(user.username, msg));
+    });
+    
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+         
+        if(user) {
+            io.to(user.room)
+              .emit('message', formatMessage(botName, `${user.username} has left the chat`));
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+    });
+});
+
+const PORT = 5000 || process.env.PORT;
+
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
